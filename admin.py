a17557c35fa561980c0742ceafa5cdd2f708de97
singh1jeet <87753055+singh1jeet@users.noreply.github.com>
2022-04-20 14:44:22 +0100

    Variables

---
+++ admin.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import pdb
+from database import *
+import tksheet
+
+label_one = None
+label_two = None
+label_three = None
+label_four = None
+input_1 = None
+input_2 = None
+input_3 = None
+input_4 = None
+listbox = None
+button_ = None
+label_five, input_5, label_six, input_6, label_seven, input_7 = None, None, None, None, None, None
+add_dress_button, show_dress_button, show_booking_button, show_customer_button, remove_dress_button, cancle_booking_button = None, None, None, None, None, None
+
+label_eight, input_8 = None, None
+
+
